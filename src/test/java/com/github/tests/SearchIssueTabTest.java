92a538be196a1edd194147c09c9428d26a57915c
berlioz458 <berlioz458@gmail.com>
2022-01-16 23:03:01 +0700

    homework done

--- src/test/java/com/github/tests/SearchIssueTabTest.java
+++ src/test/java/com/github/tests/SearchIssueTabTest.java
@@ -1,9 +1,13 @@
 package com.github.tests;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.pages.MainPage;
 import com.github.pages.RepoPage;
 import com.github.pages.SearchResultsPage;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -20,6 +24,10 @@ public class SearchIssueTabTest {
     RepoPage repoPage = new RepoPage();
 
     @Test
+    @Owner("berlioz458")
+    @Feature("IssueTab")
+    @Story("Проверка вкладки Issue в репозитории (Лямбда шаги через step)")
+    @Severity(SeverityLevel.BLOCKER)
     public void shouldHaveIssueTabOnRepoPageLambdaStep() {
         step("Открываем главную страницу", () -> {
             open("https://github.com");
@@ -40,9 +48,28 @@ public class SearchIssueTabTest {
     }
 
     @Test
-    public void shouldHaveIssueTabOnRepoPageWebStep() {
+    @Owner("berlioz458")
+    @Feature("IssueTab")
+    @Story("Проверка вкладки Issue в репозитории(Шаги с аннотацией)")
+    @Severity(SeverityLevel.BLOCKER)
+    public void shouldHaveIssueTabOnRepoPageAnnotatedStep() {
         mainPage.openPage().setQueryInput(REPOSITORY);
         searchResultsPage.goToRepo();
         repoPage.shouldSeeIssueTab();
+    }
+
+    @Test
+    @Owner("berlioz458")
+    @Feature("IssueTab")
+    @Story("Проверка вкладки Issue в репозитории(Чистый Selenide)")
+    @DisplayName("Проверка вкладки Issue в репозитории")
+    @Severity(SeverityLevel.BLOCKER)
+    public void shouldHaveIssueTabOnRepoPageListenerStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+        $("[name=\"q\"]").setValue(REPOSITORY).pressEnter();
+        $(By.linkText(REPOSITORY)).click();
+        $("#issues-tab").shouldBe(Condition.visible);
     }
 }
